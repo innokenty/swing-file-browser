25bf23ba3fe45e54667582ec784804ca730e3ec8
innokenty <innokenty@yandex-team.ru>
2014-03-06 02:20:01 +0400

    add - AbstractListDataListener which is an empty implementation for javax.swing.event.ListDataListener
    i wonder why there is no such implementation in the base package

---
+++ src/main/java/com/example/AbstractListDataListener.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import javax.swing.event.ListDataEvent;
+import javax.swing.event.ListDataListener;
+
+/**
+ * @author innokenty
+ */
+public abstract class AbstractListDataListener implements ListDataListener {
+
+    @Override
+    public void intervalAdded(ListDataEvent e) {
+    }
+
+    @Override
+    public void intervalRemoved(ListDataEvent e) {
+    }
+
+    @Override
+    public void contentsChanged(ListDataEvent e) {
+    }
+}
