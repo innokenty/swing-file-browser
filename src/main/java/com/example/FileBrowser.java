b76b5a0255df9ab7d0c62ae2d2d07ad9586840ef
innokenty <innokenty@yandex-team.ru>
2014-03-08 15:41:10 +0400

    ref - split FileBrowser initialization into methods

--- src/main/java/com/example/FileBrowser.java
+++ src/main/java/com/example/FileBrowser.java
@@ -14,15 +14,24 @@ public class FileBrowser extends JFrame {
 
     private LocalFileList fileList;
 
-    public FileBrowser() throws HeadlessException {
+    static {
         System.setProperty("apple.laf.useScreenMenuBar", "true");
+    }
 
+    public FileBrowser() throws HeadlessException {
+        initWindowProperties();
+        initComponents();
+    }
+
+    private void initWindowProperties() {
         setTitle(APP_TITLE);
         setSize(new Dimension(APP_WIDTH, APP_HEIGHT));
         setMinimumSize(new Dimension(APP_MIN_WIDTH, APP_MIN_HEIGHT));
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+    }
 
+    private void initComponents() {
         fileList = new LocalFileList();
         add(new JScrollPane(fileList), BorderLayout.CENTER);
         setJMenuBar(new FileBrowserMenuBar(fileList));
