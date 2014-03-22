51b4cec93752943c4d69cfb038aa620fe0b5204b
innokenty <innokenty@yandex-team.ru>
2014-03-22 17:27:52 +0400

    add - add method to get scaled image to Icon enum

--- src/main/java/com/example/Icon.java
+++ src/main/java/com/example/Icon.java
@@ -1,9 +1,13 @@
 package com.example;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
 
 /**
- * TODO fix image sizes
  * @author innokenty
  */
 public enum Icon {
@@ -15,8 +19,9 @@ public enum Icon {
     ZOOM_IN("img/zoom_in.png"),
     ZOOM_OUT("img/zoom_out.png"),
     ZOOM_EXPAND("img/zoom_expand.png"),
-    ZOOM_SHRINK("img/zoom_shrink.png"),
-    ;
+    ZOOM_SHRINK("img/zoom_shrink.png"),;
+
+    private static final int SCALE_METHOD = Image.SCALE_SMOOTH;
 
     private final String resourceName;
 
@@ -24,10 +29,23 @@ public enum Icon {
         this.resourceName = resourceName;
     }
 
+    private URL getResource() {
+        return this.getClass().getClassLoader().getResource(resourceName);
+    }
+
     public ImageIcon build() {
-        return new ImageIcon(this
-                .getClass()
-                .getClassLoader()
-                .getResource(resourceName));
+        return new ImageIcon(getResource());
+    }
+
+    public ImageIcon build(int width, int height) {
+        URL resource = getResource();
+        try {
+            BufferedImage image = ImageIO.read(resource);
+            Image scaled = image.getScaledInstance(width, height, SCALE_METHOD);
+            return new ImageIcon(scaled);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return build();
+        }
     }
 }
