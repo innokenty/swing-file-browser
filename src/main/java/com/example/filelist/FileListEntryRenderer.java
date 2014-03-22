926e059d69cc27d6ceb661b442f771fd3fa33cf1
innokenty <innokenty@yandex-team.ru>
2014-03-22 15:32:34 +0400

    add - add fancy file icons

--- src/main/java/com/example/filelist/FileListEntryRenderer.java
+++ src/main/java/com/example/filelist/FileListEntryRenderer.java
@@ -2,6 +2,12 @@ package com.example.filelist;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+import static javax.swing.SwingConstants.LEFT;
+import static org.apache.commons.io.FilenameUtils.getExtension;
 
 /**
  * @author innokenty
@@ -11,12 +17,12 @@ class FileListEntryRenderer implements ListCellRenderer<FileListEntry> {
     @Override
     public Component getListCellRendererComponent(
             JList<? extends FileListEntry> list,
-            FileListEntry value,
+            FileListEntry file,
             int index,
             boolean isSelected,
             boolean cellHasFocus
     ) {
-        JLabel label = new JLabel(value.getName());
+        JLabel label = new JLabel(file.getName(), getIcon(file), LEFT);
         if (isSelected) {
             label.setOpaque(true);
             label.setBackground(list.getSelectionBackground());
@@ -24,5 +30,24 @@ class FileListEntryRenderer implements ListCellRenderer<FileListEntry> {
         }
 
         return label;
+    }
+
+    private static Icon getIcon(FileListEntry file) {
+        Icon icon;
+        if (file.isDirectory()) {
+            icon = UIManager.getDefaults().getIcon("FileView.directoryIcon");
+        } else {
+            try {
+                File tempFile = File.createTempFile(
+                        UUID.randomUUID().toString(),
+                        "." + getExtension(file.getName())
+                );
+                tempFile.deleteOnExit();
+                icon = new JFileChooser().getIcon(tempFile);
+            } catch (IOException e) {
+                icon = UIManager.getDefaults().getIcon("FileView.fileIcon");
+            }
+        }
+        return icon;
     }
 }
